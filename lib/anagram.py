bf8af6dd700c53e4b4193c7f2dc70d4553831913
Ryan mugei <Lquid1264@gmail.com>
2025-05-18 20:51:08 +0300

    anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()  
+        self.sorted_word = sorted(self.word)
+
+    def match(self, candidates):
+        matches = []
+        for candidate in candidates:
+            normalized = candidate.lower()
+            if normalized != self.word and sorted(normalized) == self.sorted_word:
+                matches.append(candidate)  
+        return matches
